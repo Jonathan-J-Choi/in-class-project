a167a8601783fd0588a05f2bd7ceb1d097d167c5
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-21 10:51:07 -0500

    get working

--- server.js
+++ server.js
@@ -29,6 +29,57 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${dbName}`, {
 //       res.json(err);
 //     });
 // });
+// db.Library.create({ name: "Campus Library" })
+//   .then(dbLibrary => {
+//     console.log(dbLibrary);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//   });
+
+// app.post("/submit", ({ body }, res) => {
+//   db.Book.create(body)
+//     .then(({ _id }) =>
+//       db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true })
+//     )
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+app.get("/course", (req, res) => {
+  db.Course.find({})
+    .then(dbCourse => {
+      res.json(dbCourse);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// app.get("/library", (req, res) => {
+//   db.Library.find({})
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/populated", (req, res) => {
+//   db.Library.find({})
+//     .populate("books")
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

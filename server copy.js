65331501d3f84a9d78f1fcad3ac96c1204f10b26
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-21 10:27:14 -0500

    added server and html

---
+++ server copy.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const User = require("./userModel.js");
+const app = express();
+const dbName = "suckitgar";
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Connecting to MongoDB via mongoose
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${dbName}`, {
+  useNewUrlParser: true
+});
+
+// app.post("/submit", ({ body }, res) => {
+//   User.create(body)
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
